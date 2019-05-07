56a8afa0be1d6828d65521e1ae666d0ce36c427c
shutsugan <muhammad@nextmedia.ma>
2019-05-07 01:27:44 +0000

    feat (upload): add upload file module
    ability to upload image
    and save name to db

---
+++ src/graphql/upload.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import { join } from 'path';
+import * as shortid from 'short-id';
+
+const storeUpload = (createReadStream, filename) => {
+    const name = `${shortid.generate()}${filename}`
+    const path = `${join(__dirname, '../uploads')}/${name}`;
+    
+    return new Promise((resolve, reject) => {
+        const stream = createReadStream();
+        const writeStream = fs.createWriteStream(path);
+
+        stream.pipe(writeStream)
+            .on('finish', _ => resolve({ name }))
+            .on('error', reject);
+    });
+};
+
+const processUpload = async upload => {
+    const { createReadStream, filename } = await upload.pop();
+    const { name } = await storeUpload(createReadStream, filename);
+
+    return name;
+};
+
+export default processUpload;
