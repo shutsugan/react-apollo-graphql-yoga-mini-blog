a3d78e1797938d0cf079e6ed37940526a287ab30
shutsugan <muhammad@nextmedia.ma>
2019-05-09 23:38:05 +0000

    feat (head): create a nav head component

---
+++ client/src/components/Head/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { getUserId, removeToken } from '../../utils';
+
+const Head = _ => (
+  <div className="head flex space-between full pd-16">
+    <div className="head__left flex center">
+      <Link className="link mrr-16" to="/">Home</Link>
+      {
+        getUserId() &&
+        <Link className="link mrr-16" to="/create">Add Post</Link>
+      }
+    </div>
+    <div className="head__right">
+      {
+        getUserId() &&
+        <div className="link" onClick={_ => removeToken()}>Logout</div>
+      }
+      {
+        !getUserId() && [
+          <Link key="login" className="link mrr-16" to="/login">Login</Link>,
+          <Link key="signup" className="link mrr-16" to="/signup">Signup</Link>
+        ]
+      }
+    </div>
+  </div>
+);
+
+export default Head;
