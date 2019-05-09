83eb91a993c2fc589b9228d487b9795cdea2d2fb
shutsugan <muhammad@nextmedia.ma>
2019-05-09 23:43:21 +0000

    chore (util): add removetoken helper

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -5,4 +5,5 @@ import { AUTH_TOKEN } from "../constants";
 export const goBack = (history, path) => history.push(path);
 export const setToken = token => localStorage.setItem(AUTH_TOKEN, token);
 export const getToken = _ => localStorage.getItem(AUTH_TOKEN);
+export const removeToken = _ => localStorage.removeItem(AUTH_TOKEN);
 export const getUserId = _ => jwt.decode(getToken());
