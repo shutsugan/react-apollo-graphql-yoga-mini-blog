aa995501da03862a29058edbedb155fde060dba2
shutsugan <muhammad@nextmedia.ma>
2019-05-10 22:13:19 +0000

    feat (utils): add redirection to removeToken

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -3,7 +3,10 @@ import jwt from 'jsonwebtoken'
 import { AUTH_TOKEN } from "../constants";
 
 export const goBack = (history, path) => history.push(path);
+export const getUserId = _ => jwt.decode(getToken());
 export const setToken = token => localStorage.setItem(AUTH_TOKEN, token);
 export const getToken = _ => localStorage.getItem(AUTH_TOKEN);
-export const removeToken = _ => localStorage.removeItem(AUTH_TOKEN);
-export const getUserId = _ => jwt.decode(getToken());
+export const removeToken = (history, path) => {
+  localStorage.removeItem(AUTH_TOKEN);
+  goBack(history, path);
+};
