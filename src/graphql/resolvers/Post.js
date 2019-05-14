85b6c23a0f7818aea122af02b50be8aaac208cdd
shutsugan <muhammad@nextmedia.ma>
2019-05-14 15:23:49 +0000

    chore (mutation): update post mutation resolver

--- src/graphql/resolvers/Post.js
+++ src/graphql/resolvers/Post.js
@@ -4,9 +4,22 @@ const author = async ({ id }, _, { context }) => {
     .findById(id)
     .populate('author');
 
-  if (!author) throw new Error('No author found');
+  if (!post) throw new Error('No author found');
 
   return post.author;
 }
 
-export default { author };
+const votes = async ({ id }, _, { context }) => {
+  const post = await context.models
+    .Post.findById(id)
+    .populate('votes');
+
+    console.log(post);
+    //TODO: finished the vote resolver
+
+    if (!post) throw new Error('No votes found');
+
+    return post.votes;
+};
+
+export default { author, votes };
