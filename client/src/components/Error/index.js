4c5fbc262b2d3d2eff25a8fd7098599b8ea64e0a
shutsugan <muhammad@nextmedia.ma>
2019-05-17 02:04:59 +0000

    style (error): move the error above all

--- client/src/components/Error/index.js
+++ client/src/components/Error/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import './index.css';
 
 const Error = ({message}) => (
-    <div 
-        className="error absolute top-right pd-16"
+    <div
+        className="error fixed top-right pd-16"
         onClick={({target}) => target.remove()}>
         {message}
     </div>
