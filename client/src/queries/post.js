a53eef62e766b293b81063197643f8d79165357e
shutsugan <muhammad@nextmedia.ma>
2019-05-17 02:07:40 +0000

    refactor (post query): clean code

--- client/src/queries/post.js
+++ client/src/queries/post.js
@@ -15,6 +15,11 @@ export const POSTS_QUERY = gql`
                     name
                     email
                 }
+                votes {
+                  author {
+                    id
+                  }
+                }
             }
             count
         }
