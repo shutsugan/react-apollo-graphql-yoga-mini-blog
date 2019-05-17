30b10928e8b918a6f8f2c629e7a408ae2092bad0
shutsugan <muhammad@nextmedia.ma>
2019-05-17 02:08:00 +0000

    chore (vote query): add vote getter

--- client/src/queries/vote.js
+++ client/src/queries/vote.js
@@ -1,8 +1,21 @@
 import gql from 'graphql-tag';
 
+export const VOTE_QUERY = gql`
+  query getVote($post: String!, $author: String!) {
+    votes(post: $post, author: $author) {
+      post {
+        id
+      }
+      author {
+        id
+      }
+    }
+  }
+`;
+
 export const VOTE_MUTATION = gql`
-  mutation VoteMutation($post: ID!, $author: ID!) {
-    vote(post: $post, author: $author) {
+  mutation VoteMutation($post: String!, $author: String!) {
+    createVote(post: $post, author: $author) {
       post {
         id
         title
