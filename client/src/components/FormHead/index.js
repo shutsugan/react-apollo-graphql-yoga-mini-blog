642bfd1080f72add34c015f71a38398f0b198b98
shutsugan <muhammad@nextmedia.ma>
2019-05-30 15:47:48 +0000

    refactor (form head): clean code

--- client/src/components/FormHead/index.js
+++ client/src/components/FormHead/index.js
@@ -3,9 +3,7 @@ import React from 'react';
 const FormHead = ({title, subTitle}) => (
     <>
         <h2 className="title center mr-none">{title}</h2>
-        <h3 className="sub-title mr-none mrb-16">
-            {subTitle}
-        </h3>
+        <h3 className="sub-title mr-none mrb-16">{subTitle}</h3>
     </>
 );
 
