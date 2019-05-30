e450135f0ca55a3a79193a703bf61cd7a66df1c9
shutsugan <muhammad@nextmedia.ma>
2019-05-30 16:17:49 +0000

    feat (loader): create loader component

---
+++ client/src/components/Loader/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './index.css';
+
+const Loader = _ => (
+  <div className="loader">
+    <div className="lds-grid">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+);
+
+export default Loader;
