271dbbdb0836a5aa105d5d3c49781a3f833efa6c
shutsugan <muhammad@nextmedia.ma>
2019-05-30 15:45:43 +0000

    refactor (util): extract display error to util

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -6,7 +6,16 @@ export const goBack = (history, path) => history.push(path);
 export const getUserId = _ => jwt.decode(getToken());
 export const setToken = token => localStorage.setItem(AUTH_TOKEN, token);
 export const getToken = _ => localStorage.getItem(AUTH_TOKEN);
+
 export const removeToken = (history, path) => {
   localStorage.removeItem(AUTH_TOKEN);
   goBack(history, path);
 };
+
+export const displayError = (error, setter) => {
+  let message = (error[0])
+    ? error[0].message
+    : 'Something went wrong';
+
+  setter(message);
+}
