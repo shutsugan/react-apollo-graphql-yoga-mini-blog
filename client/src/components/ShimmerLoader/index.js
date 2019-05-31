793403c17fffe70badbefae6ce5a06e138d23816
shutsugan <muhammad@nextmedia.ma>
2019-05-31 12:04:28 +0000

    style (shimmer): add some rules

--- client/src/components/ShimmerLoader/index.js
+++ client/src/components/ShimmerLoader/index.js
@@ -5,11 +5,13 @@ import './index.css';
 const ShimmerLoader = _ => (
   <div className="shimmer__container pd-16">
     <div className="shimmer shadow flex flex-column">
-      <div className="shimmer__art shimmer full mrb-16"></div>
-      <h2 className="shimmer__title shimmer mr-none mrb-16"></h2>
-      <div className="shimmer__article shimmer"></div>
-      <div className="shimmer__article shimmer"></div>
-      <div className="shimmer__article shimmer"></div>
+      <div className="shimmer__art shimmer full"></div>
+      <div className="shimmer__info pd-16">
+        <div className="shimmer__title shimmer mr-none mrb-16"></div>
+        <div className="shimmer__article shimmer mrb-8"></div>
+        <div className="shimmer__article shimmer mrb-8"></div>
+        <div className="shimmer__article shimmer mrb-8"></div>
+      </div>
     </div>
   </div>
 );
