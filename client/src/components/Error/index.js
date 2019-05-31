8f6693f02d684d10e7297c51fbf75fb13bb4417d
shutsugan <muhammad@nextmedia.ma>
2019-05-31 12:01:21 +0000

    fix (error): extract message from error obj

--- client/src/components/Error/index.js
+++ client/src/components/Error/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import './index.css';
 
-const Error = ({message}) => (
-    <div
-        className="error fixed top-right pd-16"
-        onClick={({target}) => target.remove()}>
-        {message}
-    </div>
-);
+const Error = ({ error }) => {
+  const {message} = error;
+
+  return (
+      <div
+          className="error fixed top-right pd-16"
+          onClick={({target}) => target.remove()}>
+          {message}
+      </div>
+  );
+}
 
 export default Error;
