f496220ce286eed3e9bf077184db5aaf5ba343bc
shutsugan <muhammad@nextmedia.ma>
2019-06-03 11:17:37 +0000

    chore (app): add react-snap

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
@@ -52,11 +52,21 @@ const client = new ApolloClient({
     cache: new InMemoryCache()
 });
 
-ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById('root')
-);
+const root = document.querySelector ('#root');
+if (root.hasChildNodes()) {
+  hydrate(
+      <ApolloProvider client={client}>
+          <App />
+      </ApolloProvider>,
+      root
+  );
+} else {
+  render(
+      <ApolloProvider client={client}>
+          <App />
+      </ApolloProvider>,
+      root
+  );
+}
 
 serviceWorker.unregister();
